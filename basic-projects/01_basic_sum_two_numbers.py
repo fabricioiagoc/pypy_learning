04c23b7ca2e9bb520ceded1fbe92746bf34e413d
fabricioiagoc <fabricioiago.c@gmail.com>
2025-11-05 22:15:59 -0300

    Add first project

---
+++ basic-projects/01_basic_sum_two_numbers.py
@@ -0,0 +1,12 @@
+#This is a calculator of two numbers that i will ask the user
+
+
+#ask numbers
+n1 = float(input("Please, write the FIRST number: "))
+n2 = float(input("Please: write the SECOND number: "))
+
+
+#sum the numbers and print it
+sum = n1 + n2
+print("The sum of {} plus {} is {}".format(n1, n2, sum))
+print(f"{sum}") #This is the test for f-string
